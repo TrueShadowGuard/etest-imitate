57b0f5d16ab25bfeab0043b4d3a373c21dc4c2a0
Vadim <playvariab@gmail.com>
2022-04-29 01:35:29 +0300

    init

---
+++ index.js
@@ -0,0 +1,123 @@
+import express from "express";
+import "colors";
+import * as path from "path";
+import * as fs from "fs/promises";
+import cookieParser from "cookie-parser";
+import {parse} from 'node-html-parser';
+import {multipleTemplate, questions, singleTemplate} from "./questions.js";
+
+const PORT = 80;
+
+const app = express();
+app.use(cookieParser());
+
+let currentPage = 1;
+let visitedPages = {};
+let endDate;
+
+app.get("/restart", (req, res) => {
+    currentPage = 1;
+    visitedPages = {};
+    endDate = new Date((new Date).getTime() + 30*60000);
+    res.redirect("/page")
+});
+
+app.use("/page", express.static(path.resolve("static")));
+app.get("/page", async (req, res) => {
+    const queryPage = req.query.page;
+    if (queryPage) currentPage = queryPage;
+
+    console.log(visitedPages);
+
+    let file = (await fs.readFile(path.resolve("static", "base.htm"))).toString();
+    let html = parse(file);
+
+    renderQuestion(html, currentPage);
+    renderNavigation(html, currentPage, visitedPages);
+    renderTimer(html, endDate);
+
+    visitedPages[currentPage] = true;
+    res.setHeader("Content-Type", "text/html");
+    res.send(html.toString());
+});
+app.get("/next", (req, res) => {
+    currentPage++;
+    res.redirect("/page");
+});
+app.get("/prev", (req, res) => {
+    currentPage--;
+    console.log("prev")
+    res.redirect("/page");
+});
+
+
+function renderQuestion(html, currentPage) {
+    const model = questions[currentPage];
+
+    const template = model.type === "single" ? singleTemplate : multipleTemplate;
+
+    html.querySelector(".ablock").innerHTML = template.ablock;
+
+    const $textQuestion = html.querySelector(".qtext > p");
+    $textQuestion.textContent = model.question;
+
+    const $answer = html.querySelector(".answer");
+    for (let variant of model.variants) {
+        const $variant = parse(template.variant);
+        console.log($variant)
+        // const $label = $variant.querySelector("label");
+        // const id = Math.random();
+        // $label.setAttribute("for", id);
+        // $variant.setAttribute("id", id);
+        $variant.querySelector(".variant-text").textContent = variant;
+        $answer.innerHTML += $variant;
+    }
+
+    html.querySelector(".qno").textContent = currentPage;
+}
+
+function renderNavigation(html, currentPage, visitedPages) {
+    const $qnButtons = html.querySelector(".qn_buttons");
+    const qnButtons = $qnButtons.querySelectorAll(".qnbutton");
+    qnButtons[currentPage - 1].classList.add("thispage");
+    html.querySelector("head").innerHTML += `
+    <script>
+    document.addEventListener("click", (e) => {
+        const isInsideQnButton = e.target.classList.contains("qnbutton") || e.target.closest(".qnbutton"); 
+        if(!isInsideQnButton) return;
+        const $qnbutton = e.target.classList.contains("qnbutton") ? e.target : e.target.closest(".qnbutton");
+        e.stopImmediatePropagation();
+        e.preventDefault();
+        const questionIndex = $qnbutton.dataset.quizPage;
+        console.log(e.target.dataset);
+        window.open("/page?page=" + (+questionIndex + 1), "_self");
+    }, true);
+    </script>`;
+    if(currentPage == 1) {
+        html.querySelector("body").innerHTML += `
+        <script>       
+        document.querySelector(".mod_quiz-prev-nav").hidden = true;
+        </script>"
+        `
+    }
+    html.querySelector("head").innerHTML += `
+    <script>
+    document.addEventListener("click", e => {
+       if(!e.target.classList.contains("mod_quiz-prev-nav")) return;
+       e.preventDefault();
+       e.stopImmediatePropagation();
+       window.open("/prev", "_self");
+    }, true);
+    </script>`
+    Object.keys(visitedPages).forEach(index => {
+        qnButtons[index - 1].classList.add("answersaved");
+        qnButtons[index - 1].classList.remove("notyetanswered");
+    });
+}
+
+function renderTimer(html, endDate) {
+    let $timer = html.querySelector("#quiz-time-left");
+    $timer.setAttribute("data-end-date", +endDate)
+}
+
+app.listen(80, () => console.log(`Server started at ${PORT}`.yellow));
